4f875db9e070b7e5f92742e2a0ee51749b1c4252
scialuppa2 <scialuppa2@gmail.com>
2023-11-02 16:33:44 +0100

    aggiornamento esercizio

--- D3.js
+++ D3.js
@@ -152,7 +152,7 @@ console.log(eyeColor)
 /* ESERCIZIO 6
   Usa un while loop per calcolare la massa totale dell'equipaggio. Salvala in una variabile chiamata "crewMass".
 */
-let crewMass = 0
+let crewMass = 800
 
 /* ESERCIZIO 7
   Crea uno if/else statement per rivelare la tipologia di carico, utilizzando la massa totale, di un'ipotetica astronave contenente i personaggi dell'array "starWarsCharacters".
@@ -165,6 +165,23 @@ let crewMass = 0
 
   Una volta fatto, modifica la massa di qualche elemento dell'equipaggio e vedi se riesci ad ottenere un messaggio diverso.
 */
+if(crewMass < 500){
+  console.log("Ship is under loaded")
+} 
+else if(crewMass > 500){
+  console.log("Ship is half loaded")
+}
+else if(crewMass > 700){
+  console.log("Warning: Load is over 700")
+}
+else if(crewMass > 900){
+  console.log("Critical Load: Over 900")
+}
+else if(crewMass > 1000){
+  console.log("DANGER! OVERLOAD ALERT: escape from ship now!")
+}
+  
+
 
 /* ESERCIZIO 8
   Usa un for loop per cambiare il valore della proprietà "gender" di alcuni personaggi dal valore "n/a" a "robot" (Tip: puoi effettuare la riassegnazione del valore corrispondente o creare un nuovo array)
